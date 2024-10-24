c69bd3c95cfea5f52ca1a7fd0600193489769827
ScrawnyGrandpa <nastia1989@gmail.com>
2024-10-24 10:05:12 +0300

    Morgan Logger added

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,25 @@
 import express from "express";
 import chalk from 'chalk';
-import cors from "cors"
+import cors from "cors";
+import morgan from 'morgan';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
 import { connectToDB } from "./database/databaseServices.js";
 import { handleError } from "./utils/handleErrors.js";
 import { router } from "./router.js";
 import "dotenv/config";
 import { populateInitialData } from "./utils/populateDatabase.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+app.use(morgan('combined', { stream: accessLogStream }));
 app.use(cors());
 app.use(express.json());
 
