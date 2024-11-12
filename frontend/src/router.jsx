6a0826299f642c8ae3cbc05b0705d0b4acc42e6c
ScrawnyGrandpa <nastia1989@gmail.com>
2024-11-12 10:45:53 +0200

    Temporary router changes alongside finalized changes.

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -5,19 +5,43 @@ import S1Dungeons from "./pages/dungeons/S1Dungeons";
 import TwwDungeons from "./pages/dungeons/TwwDungeons";
 import Raids from "./pages/raids/Raids";
 import NerubarPalaceRaid from "./pages/raids/NerubarPalaceRaid";
+import BossPage from "./pages/bosses/BossPage";
+import BossForm from "./pages/bosses/BossForm";
+import BossGuide from "./pages/bosses/BossGuide";
+import RegisterPage from "./pages/user/RegisterPage";
+import LoginPage from "./pages/user/LoginPage";
 
 
 export const ROUTES = {
     ROOT: "/",
     ABOUT: "/about",
     RAIDS: "/raids",
+    BOSS_INFO: "/boss",
+    BOSS_FORM: "/boss-form",
+    BOSS_GUIDE: "/boss-guide",
     NERUBAR: "/raids/nerubar",
-    TWWDUNGEONS: "/tww-dungs",
-    S1DUNGEONS: "/s1-dungs",
+    TWWDUNGEONS: "/tww-dungeons",
+    S1DUNGEONS: "/s1-dungeonss",
     FAVORITES: "/liked-items",
     SIGNUP: "/signup",
     LOGIN: "/login",
     USER_PROFILE: "/user-profile",
+
+    // TWW Dungeons
+    ARA_KARA_ECHOES: "/tww-dungeons/ara-kara-city-of-echoes",
+    CITY_OF_THREADS: "/tww-dungeons/city-of-threads",
+    MEADERY: "/tww-dungeons/cinderbrew-meadery",
+    DARKFLAME_CLEFT: "/tww-dungeons/darkflame-cleft",
+    PRIORY: "/tww-dungeons/priory-of-the-sacred-flame",
+    THE_DAWNBREAKER: "/tww-dungeons/the-dawnbreaker",
+    THE_ROOKERY: "/tww-dungeons/the-rookery",
+    THE_STONEVAULT: "/tww-dungeons/the-stonevault",
+
+    // S1 Dungeons
+    GRIM_BATOL: "/s1-dungeons/grim-batol",
+    SIEGE: "/s1-dungeons/siege-of-boralus",
+    MISTS: "/s1-dungeons/mists-of-tirna-scithe",
+    NECROTIC_WAKE: "/s1-dungeons/the-necrotic-wake"
 };
 
 export default function Router() {
@@ -30,9 +54,12 @@ export default function Router() {
             <Route path={ROUTES.NERUBAR} element={<NerubarPalaceRaid />} />
             <Route path={ROUTES.TWWDUNGEONS} element={<TwwDungeons />} />
             <Route path={ROUTES.S1DUNGEONS} element={<S1Dungeons />} />
+            <Route path={ROUTES.BOSS_INFO + "/:id"} element={<BossPage />} />
+            <Route path={ROUTES.BOSS_FORM + "/:id"} element={<BossForm />} />
+            <Route path={ROUTES.BOSS_GUIDE + "/:id"} element={<BossGuide />} />
+            <Route path={ROUTES.SIGNUP} element={<RegisterPage />} />
+            <Route path={ROUTES.LOGIN} element={<LoginPage />} />
             {/*<Route path={ROUTES.FAVORITES} element={< />} />
-            <Route path={ROUTES.SIGNUP} element={< />} />
-            <Route path={ROUTES.LOGIN} element={< />} />
             <Route path={ROUTES.USER_PROFILE} element={< />} /> 
             {/*<Route path='*' element={<ErrorPage />} />*/}
         </Routes>
