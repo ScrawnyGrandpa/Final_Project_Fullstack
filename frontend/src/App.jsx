c9327b11ce398ceb8c4f400ad9737ec9caa11549
ScrawnyGrandpa <nastia1989@gmail.com>
2024-11-12 10:53:20 +0200

    Added providers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
 import Layout from "./components/layout/Layout";
+import PageUIProvider from "./providers/PageUIProvider";
+import AuthenticationProvider from "./providers/AuthenticationProvider";
 
 export default function App() {
 
   return (
     <BrowserRouter>
-      <Layout>
-        <Router />
-      </Layout>
+      <PageUIProvider>
+        <AuthenticationProvider>
+          <Layout>
+            <Router />
+          </Layout>
+        </AuthenticationProvider>
+      </PageUIProvider>
     </BrowserRouter>
   )
 }
