eaa6ec33a82aad4f7887ce3c27cdc007d9d06059
ScrawnyGrandpa <nastia1989@gmail.com>
2024-11-12 10:47:55 +0200

    Register & Login

---
+++ frontend/src/pages/user/LoginPage.jsx
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+import UserModel from "../../models/UserModel";
+import { useAuthentication } from "../../providers/AuthenticationProvider";
+import { useLoadCallback, usePageUI } from "../../providers/PageUIProvider";
+import PageContent from "../../components/layout/PageContent";
+import Form from "../../components/Form/Form";
+import { ROUTES } from "../../router";
+import LoginSchema from "../../schema/LoginSchema";
+
+export default function RegisterPage() {
+    const [defaultValue, setDefaultValue] = useState(new UserModel().toObject());
+    const schema = useMemo(() => new LoginSchema(), []);
+    const { user, login } = useAuthentication();
+    const { setNotification } = usePageUI();
+    const navigate = useNavigate();
+
+    const onCancel = useCallback(() => {
+        console.log("Clicked Cancel");
+        navigate(ROUTES.ROOT)
+    }, []);
+
+    const onSubmit = useLoadCallback(async ({ email, password }) => {
+        setDefaultValue({ email, password });
+        await login(email, password);
+        setNotification({ message: "Logged In", severity: "success" });
+    }, []);
+
+
+    return (
+        <PageContent>
+            {user && <Navigate to={ROUTES.ROOT} replace />}
+            {!user &&
+                <div className="max-w-4xl mx-auto p-6 w-[90vw]">
+                    <Form title="Login" {...{ defaultValue, schema, onCancel, onSubmit }}
+                    />
+                </div>
+            }
+        </PageContent>
+    );
+}
---
+++ frontend/src/pages/user/RegisterPage.jsx
@@ -0,0 +1,51 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+import UserModel from "../../models/UserModel";
+import RegisterSchema from "../../schema/RegisterSchema";
+import { useAuthentication } from "../../providers/AuthenticationProvider";
+import { useLoadCallback, usePageUI } from "../../providers/PageUIProvider";
+import PageContent from "../../components/layout/PageContent";
+import Form from "../../components/Form/Form";
+import { ROUTES } from "../../router";
+
+export default function RegisterPage() {
+    const [defaultValue, setDefaultValue] = useState(new UserModel().toObject());
+    const schema = useMemo(() => new RegisterSchema(), []);
+    const { user, login } = useAuthentication();
+    const { setNotification } = usePageUI();
+    const navigate = useNavigate();
+
+    const onCancel = useCallback(() => {
+        console.log("Clicked Cancel");
+        navigate(ROUTES.ROOT)
+    }, []);
+
+    const onSubmit = useLoadCallback(async data => {
+        const user = new UserModel(data);
+        setDefaultValue(data);
+
+        const newUser = user.cleanup();
+        console.log("saved newUser", newUser);
+
+        await newUser.save();
+        console.log("Saving to Databse");
+
+
+        setNotification({ message: "Registration complete", severity: "success" });
+        await login(newUser.email, newUser.password);
+    }, []);
+
+
+    return (
+        <PageContent>
+            {user && <Navigate to={ROUTES.ROOT} replace />}
+            {!user &&
+                <div className="max-w-4xl mx-auto p-6 w-[90vw]">
+                    <Form title="Register" {...{ defaultValue, schema, onCancel, onSubmit }}
+                    />
+                </div>
+            }
+        </PageContent>
+    );
+}
