6fd3229eb78960b60214e4ba00661dab9e80e361
ScrawnyGrandpa <nastia1989@gmail.com>
2024-11-12 10:51:36 +0200

    Boss Guide Edit Page - important !!!

---
+++ frontend/src/pages/bosses/BossGuide.jsx
@@ -0,0 +1,158 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useLoadEffect } from '../../providers/PageUIProvider';
+import BossModel from '../../models/BossModel';
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import { SPELL, NPC } from "../../utils/wowheadLinks";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+import GuideForm from '../../components/Form/GuideForm';
+
+export default function BossGuide() {
+    const [boss, setBoss] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+    const [orderedNormalPhases, setOrderedNormalPhases] = useState([]);
+    const [orderedHeroicPhases, setOrderedHeroicPhases] = useState([]);
+    const navigate = useNavigate();
+    const [newAddition, setNewAddition] = useState(false);
+
+    useLoadEffect(async () => {
+        const boss = await BossModel.load(id);
+        setBoss(boss);
+    }, [id]);
+
+    useEffect(() => {
+        if (boss) {
+            setOrderedNormalPhases(boss.guide.normal || []);
+            setOrderedHeroicPhases(boss.guide.heroic || []);
+        }
+        setLoading(false);
+    }, [boss]);
+
+    useEffect(() => {
+        if (boss) {
+            WH.Tooltips.refreshLinks();
+        }
+        setLoading(false);
+    }, [boss]);
+
+    const handleDragEnd = (result, type) => {
+        const { destination, source } = result;
+        if (!destination) return;
+
+        const reorder = (list, startIndex, endIndex) => {
+            const [removed] = list.splice(startIndex, 1);
+            list.splice(endIndex, 0, removed);
+            return [...list];
+        };
+
+        let updatedPhases;
+        if (type === "normal") {
+            updatedPhases = reorder(orderedNormalPhases, source.index, destination.index);
+            setOrderedNormalPhases(updatedPhases);
+        } else if (type === "heroic") {
+            updatedPhases = reorder(orderedHeroicPhases, source.index, destination.index);
+            setOrderedHeroicPhases(updatedPhases);
+        }
+    };
+
+    const submitGuide = async () => {
+        const updatedBoss = new BossModel({
+            ...boss,
+            guide: { normal: orderedNormalPhases, heroic: orderedHeroicPhases }
+        });
+
+        console.log("Submitting form with updated guide data", updatedBoss);
+
+        await updatedBoss.save();
+        setNewAddition(true);
+    };
+
+    if (loading || !boss) return <div>Loading...</div>;
+
+    return (
+        <div className="container mx-auto px-4 max-w-4xl bg-[#202020] rounded-sm">
+            {/* Render the guide form for admin editing */}
+            <GuideForm boss={boss} initialGuide={boss.guide} submitGuide={submitGuide} />
+            <div className="mx-3">
+                {/* Drag-and-drop for normal phases */}
+                <h3 className="text-cyan-300 text-xl font-bold mb-4">Normal Phases</h3>
+
+                <DragDropContext onDragEnd={(result) => handleDragEnd(result, "normal")}>
+                    <Droppable droppableId="normal-phases">
+                        {(provided) => (
+                            <div ref={provided.innerRef} {...provided.droppableProps}>
+                                {orderedNormalPhases.map((phaseInfo, index) => (
+                                    <Draggable key={index} draggableId={`normal-${index}`} index={index}>
+                                        {(provided) => (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                                className="mb-4 p-4"
+                                            >
+                                                <h2 className="text-cyan-200 text-2xl font-bold mb-4">{phaseInfo.phase}:</h2>
+                                                <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+                                                    {phaseInfo.description
+                                                        .replace(/\[(\d+)\]/g, (match, skillId) => {
+                                                            return `<a href="${SPELL}${skillId}" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">${skillId}</a>`;
+                                                        })
+                                                        .replace(/{(\d+)}/g, (match, npcId) => {
+                                                            return `<a href="${NPC}${npcId}" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">${npcId}</a>`;
+                                                        })
+                                                        .replace(/\n/g, '<br />')}
+                                                </ReactMarkdown>
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+
+                {/* Drag-and-drop for heroic phases */}
+                <h3 className="text-cyan-300 text-xl font-bold mb-4">Heroic Phases</h3>
+                <DragDropContext onDragEnd={(result) => handleDragEnd(result, "heroic")}>
+                    <Droppable droppableId="heroic-phases">
+                        {(provided) => (
+                            <div ref={provided.innerRef} {...provided.droppableProps}>
+                                {orderedHeroicPhases.map((heroicPhase, heroicIndex) => (
+                                    <Draggable key={heroicIndex} draggableId={`heroic-${heroicIndex}`} index={heroicIndex}>
+                                        {(provided) => (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                                className="mb-4 p-4 bg-[#lalala]"
+                                            >
+                                                <h4 className="text-cyan-200 font-semibold">{heroicPhase.phase}:</h4>
+                                                <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+                                                    {heroicPhase.description
+                                                        .replace(/\[(\d+)\]/g, (match, skillId) => {
+                                                            return `<a href="${SPELL}${skillId}" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">${skillId}</a>`;
+                                                        })
+                                                        .replace(/{(\d+)}/g, (match, npcId) => {
+                                                            return `<a href="${NPC}${npcId}" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">${npcId}</a>`;
+                                                        })
+                                                        .replace(/\n/g, '<br />')}
+                                                </ReactMarkdown>
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
+
+                <button onClick={submitGuide} className="my-10 bg-blue-500 text-white py-2 px-6 rounded">
+                    Save Guide
+                </button>
+            </div>
+        </div>
+    );
+}
