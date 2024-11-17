323a8c97c65db1d4fab5fcfa20499b3363abe56b
ScrawnyGrandpa <nastia1989@gmail.com>
2024-11-17 16:07:26 +0200

    Scroll To Top

--- frontend/src/components/layout/MainBody.jsx
+++ frontend/src/components/layout/MainBody.jsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function MainBody({ children }) {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+
+
     return (
         <div className="flex flex-col min-h-screen">
             <main className="flex-1 flex justify-center w-full h-full bg-[#1a1b1b] text-[white] pt-8">
