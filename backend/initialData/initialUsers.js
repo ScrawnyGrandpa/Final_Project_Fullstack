e72441dd40793a5e6f147733767cb70a02a65617
ScrawnyGrandpa <nastia1989@gmail.com>
2024-12-07 15:05:13 +0200

    Testing v2

---
+++ backend/initialData/initialUsers.js
@@ -0,0 +1,14 @@
+export const InitialUsers = [
+    // ++
+    {
+        "firstName": "Admin",
+        "lastName": "Admin",
+        "nickName": "AdminUser",
+        "email": process.env.ADMIN_EMAIL,
+        "password": process.env.ADMIN_PASSWORD,
+        "avatarImgURL": "",
+        "avatarImgALT": "",
+        "likedNPCs": [],
+        "isAdmin": true
+    }
+];
