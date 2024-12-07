5ac76d84eb0ffb952b97e78c3dd9e3a778751de7
ScrawnyGrandpa <nastia1989@gmail.com>
2024-12-07 15:11:32 +0200

    RENDER Test

--- frontend/src/services/TrashAPI.js
+++ frontend/src/services/TrashAPI.js
@@ -1,5 +1,5 @@
 import API from "./API";
 
 export default class TrashAPI extends API {
-    static apiUrl = "http://localhost:3000/trash";
+    static apiUrl = "https://final-project-fullstack.onrender.com/trash";
 }
