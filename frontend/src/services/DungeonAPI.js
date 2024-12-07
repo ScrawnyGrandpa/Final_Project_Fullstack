259b176f588fa30b826035036690584dea0c7401
ScrawnyGrandpa <nastia1989@gmail.com>
2024-12-07 15:16:03 +0200

    Update DungeonAPI.js

--- frontend/src/services/DungeonAPI.js
+++ frontend/src/services/DungeonAPI.js
@@ -1,5 +1,5 @@
 import API from "./API";
 
 export default class DungeonAPI extends API {
-    static apiUrl = "https://final-project-fullstack.onrender.com/dungeon";
+    static apiUrl = "https://final-project-fullstack-1.onrender.com/dungeon";
 }
